61f50d38d0a55092e7a65794f01e2c482e1121fa
Fergal Miller <fergalmiller@hotmail.co.uk>
2020-07-02 20:00:15 +0100

    Refactor: Expression pop and peek methods extracted common code.

--- src/exp_evaluator/expression.rs
+++ src/exp_evaluator/expression.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 pub struct Expression
 {
     pub expression: String
@@ -8,32 +10,25 @@ impl Expression
     pub fn pop(&mut self) -> Option<char>
     {
         let mut chars = self.expression.chars();
-
-        let mut c: Option<char>;
-        loop
-        {
-            c = chars.next();
-
-            if c.is_none() { return Option::None::<char>; }
-            if !c.unwrap().is_whitespace() { break; }
-        }
-
+        let c = find_next_non_whitespace(&mut chars);
         self.expression = String::from(chars.as_str());
         c
     }
 
     pub fn peek(&self) -> Option<char>
     {
-        let mut chars = self.expression.chars();
-
-        let mut c: Option<char>;
-        loop
-        {
-            c = chars.next();
-
-            if c.is_none() { return Option::None::<char>; }
-            if !c.unwrap().is_whitespace() { break; }
-        }
-        c
+        find_next_non_whitespace(&mut self.expression.chars())
     }
+}
+
+fn find_next_non_whitespace(chars: &mut Chars) -> Option<char>
+{
+    let mut c: Option<char>;
+    loop
+    {
+        c = chars.next();
+        if c.is_none() { return Option::None::<char>; }
+        if !c.unwrap().is_whitespace() { break; }
+    }
+    c
 }
